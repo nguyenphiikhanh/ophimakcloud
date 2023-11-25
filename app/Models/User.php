f0509e4a9c65af77589f8bb811a4579d9729fc9c
dev. Khanh <nguyenphiikhanh@gmail.com>
2023-11-25 21:51:43 +0700

    admin panel config

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Ophim\Core\Models\User as OphimUser;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends OphimUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
